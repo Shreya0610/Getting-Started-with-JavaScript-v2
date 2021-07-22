ca8b0afff1e1a0d50ab2b1fc155bbc671d66a1c1
Shreya Sharma <sshreyasharma53@gmail.com>
2021-07-22 22:22:40 +0530

    initial commit

---
+++ js/intro.js
@@ -0,0 +1,16 @@
+//creating variables
+var myName = "Shreya";
+var gitHubHandle = "Shreya0610";
+var age = 18;
+
+// function declaration
+function whoAmI(name, nickname, Age) {
+  console.log(`
+    Hi, I'm ${name} (aka ${nickname}), 
+    and I'm ${Age} years old.
+    `);
+}
+whoAmI(myName, gitHubHandle, age);
+
+// operator use
+console.log(myName + age);
