cc0a836f336ca77e0a303c8d91dae650cea80cb3
Shreya Sharma <sshreyasharma53@gmail.com>
2021-07-23 17:18:30 +0530

    Exercise

--- js/intro.js
+++ js/intro.js
@@ -10,7 +10,40 @@ function whoAmI(name, nickname, Age) {
     and I'm ${Age} years old.
     `);
 }
+// function calling
 whoAmI(myName, gitHubHandle, age);
 
 // operator use
 console.log(myName + age);
+
+// Array and its calling
+var friends = ["Ekta", "Shivani", "Rai"];
+console.log(friends.length);
+console.log(friends[0]);
+
+//Exercise 1 :
+
+var favouriteBooks = [];
+
+function addFavouriteBook(bookName) {
+  if (!bookName.includes("Great")) { // includes will bring out every name where "Great" keyword is used.
+    favouriteBooks.push(bookName);
+  }
+}
+
+function printFavouriteBooks() {
+  console.log(
+    `Favourite Books : ${favouriteBooks.length}`
+  )
+  for(let book of favouriteBooks){
+    console.log(book);
+  }
+}
+
+addFavouriteBook("A Song of Ice and Fire");
+addFavouriteBook("The Great Gatsby");
+addFavouriteBook("Crime & Punishment");
+addFavouriteBook("Great Expectations");
+addFavouriteBook("You Don't Know JS");
+
+printFavouriteBooks();
